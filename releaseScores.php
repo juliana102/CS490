eb5cdcf28a9ceea95b0d5dcc564cb3ad264b7ca2
Juliana Ceballos <34225683+juliana102@users.noreply.github.com>
2020-04-06 19:49:25 -0400

    Add files via upload

---
+++ releaseScores.php
@@ -0,0 +1,12 @@
+<?php
+$crl = curl_init();
+curl_setopt($crl, CURLOPT_URL, "https://web.njit.edu/~");
+curl_setopt($crl, CURLOPT_POST, 1);
+curl_setopt($crl, CURLOPT_POSTFIELDS);
+curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+//$c=json_decode(curl_exec($crl));
+$c=(curl_exec($crl));
+//echo json_encode($c);
+echo $c;
+?>
