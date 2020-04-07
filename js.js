d14e0a880a5a6cdbf6d5af3e0bb75fe5fe4dca36
Juliana Ceballos <34225683+juliana102@users.noreply.github.com>
2020-04-07 19:03:04 -0400

    Update js.js

--- js.js
+++ js.js
@@ -74,74 +74,16 @@ function makeExam() {
       if (count < 3) {
         document.getElementById("alert").innerHTML = "you created " + count + " question(s), you need to make at least 3";
         return;
-        //exit();
+        
       }
       if (points < 100) {
         document.getElementById("alert").innerHTML = document.getElementById("testname").value + " is currently " + points + "/100. Please add " + (100 - points) + " points.";
         return;
-        //exit();
+        
       }
       else if (points > 100) {
         document.getElementById("alert").innerHTML = document.getElementById("testname").value + " is currently " + points + "/100. Please subtract " + (points - 100) + " points or remove questions.";
         return;
-        //exit();
+        
       }
-    
-/* function populateQDB(response){
-	questionDB = JSON.parse(response);
- console.log(questionDB);
-	var table = document.getElementById("question_table");
- table.innerHTML="";
-	for (var i in questionDB){
-		var tr = document.createElement("tr");
-		var question_id_td = document.createElement("td");
-		var question_id = document.createTextNode(i);
-		question_id.id = "id_"+questionDB[i];
-		question_id_td.appendChild(question_id);
-
-		var question_name_td = document.createElement("td");
-		var question_name = document.createTextNode(questionDB[i]['functionName']);
-		question_name_td.appendChild(question_name);
-
-
-		var difficulty_td = document.createElement("td");
-		var difficulty = document.createTextNode(questionDB[i]['difficulty']);
-		difficulty_td.appendChild(difficulty);
-
-		var topic_td = document.createElement("td");
-		var topic = document.createTextNode(questionDB[i]['topic']);
-		topic_td.appendChild(topic);
-
-		var question_text_td = document.createElement("td");
-		var question_text = document.createTextNode(questionDB[i]['question']);
-		question_text_td.appendChild(question_text);
-
-		var add_td = document.createElement("td");
-
-		if (questions_added == 0){
-		add_td.innerHTML = '<div class="text-center" ><input class="tiny-button" type="button" value="Add" onClick="addQuestion('+i+')" id="question_to_add_'+i+'"></div>'
-		} else {
-			var flag = false;
-			for (var j in questions_added){
-				if (i == j){
-					flag = true;
-				}
-			}
-			if (flag == true) {
-			add_td.innerHTML = '<div class="text-center" ><input class="tiny-button" type="button" disabled value="Add" onClick="addQuestion('+i+')" id="question_to_add_'+i+'"></div>'
-			} else {
-			add_td.innerHTML = '<div class="text-center" ><input class="tiny-button" type="button" value="Add" onClick="addQuestion('+i+')" id="question_to_add_'+i+'"></div>'
-			}
-		}
-
-		tr.appendChild(question_id_td);
-		tr.appendChild(question_name_td);
-		tr.appendChild(difficulty_td);
-		tr.appendChild(topic_td);
-		tr.appendChild(question_text_td);
-		tr.appendChild(add_td);
-
-		table.appendChild(tr);
-	}
-
 
