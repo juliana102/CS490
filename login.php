133a32428841ee7033c9a865946fab7a1ad18453
Juliana Ceballos <34225683+juliana102@users.noreply.github.com>
2020-04-16 22:27:38 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,50 +1,18 @@
 <?php
 session_start();
-$error='';
-if (isset($_POST['submit'])) {
-if (empty($_POST['U']) || empty($_POST['P'])) {
-$error = "UCID or Password is invalid";
+
+function curl($url, $arr) {
+  $ch = curl_init();
+  curl_setopt($ch,CURLOPT_URL, $url);
+  curl_setopt($ch,CURLOPT_POST, count($arr));
+  curl_setopt($ch,CURLOPT_POSTFIELDS, $arr);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $response = trim( curl_exec($ch) );
+  curl_close($ch);
+  return $response;
 }
-else
-{
 
-$ucid=$_POST['U'];
-$password=$_POST['P'];
-$json = array('UCID' => $ucid, 'password' => $password);
-$data = json_encode($json);
-echo $data;
-$crl = curl_init();
-curl_setopt($crl, CURLOPT_URL, "https://web.njit.edu/~pz89/final.php");
-curl_setopt($crl, CURLOPT_POST, 1);
-curl_setopt($crl, CURLOPT_POSTFIELDS, $data);
-curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-$c=(curl_exec($crl));
-echo $c;
-if ($c==1){
-	$_SESSION['login_user']=$ucid;
-	$crl = curl_init();
-	curl_setopt($crl, CURLOPT_URL, "/Professor/session.php");
-	curl_setopt($crl, CURLOPT_POST, 1);
-	curl_setopt($crl, CURLOPT_POSTFIELDS, $_SESSION['login_user']);
-	curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-	header("location: https://web.njit.edu/~jac323/instructor.php");
-
-
-	}
-	elseif ($c==0){
-		$_SESSION['login_user']=$ucid;
-		header("location: https://web.njit.edu/~jac323/studentpage.php");
-
-		}
-		else{
-			$_SESSION['login_user']=$ucid;
-			header("location: index.php");
-			echo "Invalid Login";
-			}
-
-}
+  echo "UCID: ", $_SESSION['ucid'];
 }
 
 ?>
